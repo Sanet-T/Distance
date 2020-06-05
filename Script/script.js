0bf68726188861c22aea3bad53d40f282142d126
Sanet-T <61738779+Sanet-T@users.noreply.github.com>
2020-06-05 15:46:03 +0500

    Add files via upload

---
+++ Script/script.js
@@ -0,0 +1,16 @@
+const menuButton  = document.querySelector(".M_menu");
+const menu = document.querySelector('.Menu_Sidebar');
+
+
+menuButton.addEventListener('click', function () {
+    menu.classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+
